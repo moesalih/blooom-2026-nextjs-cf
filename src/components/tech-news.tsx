deb2fa12e9cf298a7c799318f7a128d6742dea78
MOΞ <moe.salih@gmail.com>
2026-05-31 20:14:21 -0400

    rename api route

--- src/components/tech-news.tsx
+++ src/components/tech-news.tsx
@@ -3,5 +3,5 @@
 import { NewsFeed } from "@/components/news-feed";
 
 export function TechNews() {
-	return <NewsFeed queryKey={["techmeme"]} apiPath="/api/techmeme" />;
+	return <NewsFeed queryKey={["techmeme"]} apiPath="/api/news/tech" />;
 }
