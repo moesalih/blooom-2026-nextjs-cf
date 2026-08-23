123cfb7e749c953c1af7c72a7710449fd80f3c5e
MOΞ <moe.salih@gmail.com>
2026-08-23 14:25:32 -0400

    shadcn tooltip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { QueryProvider } from "@/components/query-provider";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
@@ -37,7 +38,9 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistSans.className} ${geistMono.variable} antialiased`}
 			>
-				<QueryProvider>{children}</QueryProvider>
+				<TooltipProvider>
+					<QueryProvider>{children}</QueryProvider>
+				</TooltipProvider>
 			</body>
 		</html>
 	);
